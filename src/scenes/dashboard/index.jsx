52e39425d17379731da6c63e57f43c2d738e48d7
Carlos <cchen@itba.edu.ar>
2024-08-15 14:00:42 -0300

    dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,12 +1,43 @@
-import { Box } from '@mui/material';
+import { Box, Button, IconButton, Typography, useTheme } from '@mui/material';
+import { tokens } from '../../theme';
 import Header from '../../components/Header';
+import { mockTransactions } from '../../data/mockData';
+import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
+import EmailIcon from '@mui/icons-material/Email';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import TrafficIcon from '@mui/icons-material/Traffic';
+import LineChart from '../../components/LineChart';
+import BarChart from '../../components/BarChart';
+import GeographyChart from '../../components/GeographyChart';
+import StatBox from '../../components/StatBox';
+import ProgressCircle from '../../components/ProgressCircle';
 
 const Dashboard = () => {
-    return <Box m="20px">
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-            <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+            </Box>
+
+            <Box>
+                <Button
+                    sx={{ 
+                        backgroundColor: colors.blueAccent[700], 
+                        color: colors.grey[100], 
+                        fontSize:"14px", 
+                        fontWeight:"bold", 
+                        padding:"10px 20px" 
+                    }}
+                >
+                    <DownloadOutlinedIcon  />
+                </Button>
+            </Box>
         </Box>
-    </Box>
+    );
 };
 
 export default Dashboard;
